005433343dc306596efbbe61dabb9b25231e6e3c
devhumayun <humayunkabir9408@gmail.com>
2023-09-15 23:50:26 +0600

    getting data by type featured added

--- routes/layoutRoute.ts
+++ routes/layoutRoute.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { authorizeRoles, isAuthenticated } from '../middleware/auth'
-import { createlayout, editLayout } from '../controller/layoutController'
+import { createlayout, editLayout, getLayout } from '../controller/layoutController'
 const router = express.Router()
 
 // user api route
 router.post("/create-layout", isAuthenticated, authorizeRoles("admin"), createlayout)
 router.put("/edit-layout", isAuthenticated, authorizeRoles("admin"), editLayout)
+router.get("/get-layout", getLayout)
 
 
 export default router
